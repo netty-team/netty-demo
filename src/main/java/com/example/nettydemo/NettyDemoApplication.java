257de8eaab7629c4358b26812c8c40280f305ee3
lixinping <576689502@qq.com>
2021-01-28 09:19:30 +0800

    mysql驱动依赖加入

--- src/main/java/com/example/nettydemo/NettyDemoApplication.java
+++ src/main/java/com/example/nettydemo/NettyDemoApplication.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class NettyDemoApplication implements CommandLineRunner {
 
     @Value("${netty.json.port}")
